59d5148c0166951a0d671c293162200475ffecb0
franaim <franmenardi.dibujo@gmail.com>
2023-09-04 16:40:01 +0200

    First JavaScript test

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+let body = document.getElementById("quiz-container")
+let startButton = document.getElementById("start-button")
+
+startButton.addEventListener("click", slide2)
+
+function slide2() {
+    `
+    <h1>Insert Nickname</h1>
+      <input id="nickname" type="text">
+      <p id="slide2-nextButton">Next</p>
+    `
+    
+}
