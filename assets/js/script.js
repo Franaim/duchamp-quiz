a2254318d93e08d42f2e5381b6ad96c8d217184e
franaim <franmenardi.dibujo@gmail.com>
2023-09-04 17:14:06 +0200

    Username section test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
-let body = document.getElementById("quiz-container")
+let body = document.getElementById("game-area")
 let startButton = document.getElementById("start-button")
 
 startButton.addEventListener("click", slide2)
 
 function slide2() {
     `
-    <h1>Insert Nickname</h1>
-      <input id="nickname" type="text">
-      <p id="slide2-nextButton">Next</p>
+    <p class="question-area">Choose a nickname</p>
+    <form>
+      <label for="username">Player:</label><br />
+      <input type="text" id="username" name="username" required />
+    </form>
+    <p id="slide2-nextButton">Next</p>
     `
-    
 }
