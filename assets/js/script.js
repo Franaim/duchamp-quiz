ac4e79af860dc2d7a2e3367382c74fc5f3e17ddf
franaim <franmenardi.dibujo@gmail.com>
2023-09-05 10:30:12 +0200

    Correct slide0 function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,34 @@
-let body = document.getElementById("game-area")
+//html elements
+let body = document.getElementById("introduction-area")
 let startButton = document.getElementById("start-button")
 
-startButton.addEventListener("click", slide2)
+//Set the counter to 0
 
-function slide2() {
+let countTrue = 0;
+let countFalse = 0;
+
+//Event listeners
+startButton.addEventListener("click", slide0);
+startButton.addEventListener("click", showCounter);
+
+
+function slide0() {
+    body.innerHTML =
     `
-    <p class="question-area">Choose a nickname</p>
-    <form>
-      <label for="username">Player:</label><br />
-      <input type="text" id="username" name="username" required />
-    </form>
-    <p id="slide2-nextButton">Next</p>
-    `
+     <p class="question-area">Choose a nickname</p>
+        <form>
+          <label for="username">Player:</label><br />
+          <input type="text" id="username" name="username" required />
+        </form>
+        <button id="play-button">Play</button>
+    `;
+let userName = document.getElementById("username");
+let counterDiv = document.getElementById("score-area");
+
+//Event listeners
+
+let playButton = document.getElementById("play-button")
+playButton.addEventListener("click", slide1);
+startButton.addEventListener("click", showCounter);
 }
+
